9c580660daa8f9761020316b33e219d2dc06deb4
Rowan <rowanpillay@gmail.com>
2016-06-21 10:19:19 +0200

    Update BibleNode.js
    test commit

--- BibleNode.js
+++ BibleNode.js
@@ -8,7 +8,7 @@ var port = process.env.PORT || 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('Hello World - Gerhard!!!\n');
 });
 
 server.listen(port, () => {
