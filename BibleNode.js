dc6938fa0ddaa8f424c934adc3e3878571f580f2
Rowan <rowan@Rowbot>
2016-07-13 07:27:35 +0200

    Actual Telegram bot, responds with a verse

--- BibleNode.js
+++ BibleNode.js
@@ -1,4 +1,33 @@
-const http = require('http');
+'use strict'
+
+const Telegram = require('telegram-node-bot')
+const TelegramBaseController = Telegram.TelegramBaseController
+const tg = new Telegram.Telegram('145950702:AAFP2xrvD68yKHBnwXU-_xCb_cvcqxzQNMU')
+
+class VerseController extends TelegramBaseController {
+    /**
+     * @param {Scope} $
+     */
+    verseHandler($) {
+        $.sendMessage('And we know that in all things God works for the good of those who love him, who have been called according to his purpose. Romans 8:28 (NIV)')
+    }
+
+    get routes() {
+        return {
+            'verse': 'verseHandler',
+            'Verse': 'verseHandler',
+            'VERSE': 'verseHandler',
+        }
+    }
+}
+
+tg.router
+    .when(['verse'], new VerseController())
+tg.router
+    .when(['Verse'], new VerseController())
+tg.router
+    .when(['VERSE'], new VerseController())
+/*const http = require('http');
 
 
 // set the port of our application
@@ -14,3 +43,4 @@ const server = http.createServer((req, res) => {
 server.listen(port, () => {
   console.log('Server running at http');
 });
+*/
